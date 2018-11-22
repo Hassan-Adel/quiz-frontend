041ace08b02594066e6e8442f7a3f4e554cd39b4
Hassan Adel <hassan.adel@RMGS.CO>
2018-11-22 17:16:10 +0200

    Adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
 import {QuestionComponent} from './question/question.component';
 
@@ -12,7 +14,9 @@ import {QuestionComponent} from './question/question.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
